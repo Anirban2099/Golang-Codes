a93757c9988e61556552fe6913003e69931b1329
Anirban Chatterjee <ani.kol2099@gmail.com>
2024-07-15 00:25:32 +0530

    TimeHandling

--- 06Pointers/main.go
+++ 06Pointers/main.go
@@ -7,13 +7,17 @@ import (
 func main() {
 	fmt.Println("These is about Pointers class")
 
-	var ptr *int // Suggests that the pointer is working with integer value
+	var ptr *int
+	// Suggests that the pointer is working with integer value
 
 	fmt.Println("The value of the pointer is", ptr)
 
-	number1 := 77
-	var ptr1 = &number1
-	fmt.Println("The value of the 2nd pointer is", ptr1)   // Gives memory address
-	fmt.Println("The value of the 2nd  pointer is", *ptr1) // Gives integer value
+	Mynumber := 77
+	var ptr0 = &Mynumber
+	fmt.Println("The value of the 2nd pointer is", ptr0)   // Gives memory address
+	fmt.Println("The value of the 2nd  pointer is", *ptr0) // Gives integer value
+
+	*ptr0 = *ptr0 + 2
+	fmt.Println("The new added +2 number is: ", ptr0)
 
 }
