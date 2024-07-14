1baccb77f19590d3de65a77c1b589ec62f01f370
Anirban Chatterjee <ani.kol2099@gmail.com>
2024-07-15 00:09:59 +0530

    PointersHandling

--- 06Pointers/main.go
+++ 06Pointers/main.go
@@ -7,8 +7,13 @@ import (
 func main() {
 	fmt.Println("These is about Pointers class")
 
-	var 
+	var ptr *int // Suggests that the pointer is working with integer value
 
+	fmt.Println("The value of the pointer is", ptr)
 
+	number1 := 77
+	var ptr1 = &number1
+	fmt.Println("The value of the 2nd pointer is", ptr1)   // Gives memory address
+	fmt.Println("The value of the 2nd  pointer is", *ptr1) // Gives integer value
 
 }
