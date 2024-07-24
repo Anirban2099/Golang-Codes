545b92a85c7ebe516cf31d448a14ebdbdac8685e
Anirban Chatterjee <ani.kol2099@gmail.com>
2024-07-25 00:53:18 +0530

    Loops

--- 14LoopBreak/main.go
+++ 14LoopBreak/main.go
@@ -16,9 +16,23 @@ func main() {
 
 	fmt.Println("list of Pokemons: ", Pokemon)
 
+	//Process:1
 	for i := 0; i < len(Pokemon); i++ {
 		fmt.Println(Pokemon[i])
 	}
+	//Process:2
+	for p := range Pokemon {
+		fmt.Println(Pokemon[p])
+	}
+	//Process:3 (Index added to contents) if index is not needed then replace index with "_"
+	for index, Pokemon := range Pokemon {
+		fmt.Printf("Index is %v and value is %v\n", index, Pokemon)
+	}
 
-	
+	rougeValue := 1
+
+	for rougeValue < 10 {
+		fmt.Println("Value is : ", rougeValue)
+		rougeValue++
+	}
 }
